b82c483bcd86cd4c4950df27f44f1c370550c2a0
Edun Rilwan <111396734+Omotunde2005@users.noreply.github.com>
2024-08-16 23:11:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Area of a triangle = (1/2)bh
+
+function areaOfTriangle (b, h) {
+    let base = b
+    let height = h
+
+    result = (1/2 * base * height)
+    return result 
+;}
+
+console.log(areaOfTriangle(5, 2))
